205efc369b88ea25554b3f5af8d5dcfbf85cbb83
jkim333 <kimjihyung3@gmail.com>
2021-09-03 10:00:38 +1000

    Moved main.js file into a new directory called 'js'.

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ const navbarBody = document.querySelector(".navbar__body");
 
 navbarBtn.addEventListener("click", (e) => {
   navbarBody.classList.toggle("show");
-  console.log(navbarBtn.querySelector("i").className);
+
   if (navbarBtn.querySelector("i").className === "fas fa-bars") {
     navbarBtn.querySelector("i").className = "fas fa-times";
   } else {
